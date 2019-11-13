afa07ffdd46d584930c9714df960168d02ef7a58
Lance Campos <lcampos20058@columbushs.com>
2019-11-12 23:07:42 -0500

    done

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,28 +1,87 @@
+
 // Rover Object Goes Here
 // ======================
 let rover = {
-  direction:'N'
+  direction:'N', x:0, y:0, travelLog: []
 }
 // ======================
-function turnLeft(){
- 
-if (rover.direction() = 'N'){
-  rover.direction() = 'W';
-} else if (rover.direction() = 'W'){
-  rover.direction() = 'S';
-} else if (rover.direction() = 'S'){
-  rover.direction() = 'E';
-} else if (rover.direction() = 'E'){
-  rover.direction() = 'N';
+
+
+function turnLeft(rover){
+
+console.log("turnLeft was called!");
+
+if (rover.direction == 'N'){
+ rover.direction = 'W';
+} 
+else if (rover.direction == 'W'){
+ rover.direction = 'S';
+} 
+else if (rover.direction == 'S'){
+ rover.direction = 'E';
+} 
+else if (rover.direction == 'E'){
+ rover.direction = 'N';
+  }
 }
 
-  console.log("turnLeft was called!");
-}
 
 function turnRight(rover){
+ 
   console.log("turnRight was called!");
+
+  if (rover.direction == 'N'){
+    rover.direction = 'E';
+  } 
+  else if (rover.direction == 'E'){
+    rover.direction = 'S';
+  } 
+  else if (rover.direction == 'S'){
+    rover.direction = 'W';
+  } 
+  else if (rover.direction == 'W'){
+    rover.direction = 'N';
+  }
 }
 
+
 function moveForward(rover){
+
   console.log("moveForward was called")
+
+  if(rover.direction == 'N'){
+    rover.travelLog.push(rover.x , rover.y);
+    rover.y = rover.y + 1;
+    console.log( `The rover moved to ${rover.x}, ${rover.y}`);
+  } 
+  else if (rover.direction == 'E'){
+    rover.travelLog.push(rover.x , rover.y);
+    rover.x = rover.x + 1;
+    console.log( `The rover moved to ${rover.x}, ${rover.y}`);
+  } 
+  else if (rover.direction == 'S'){
+    rover.travelLog.push(rover.x , rover.y);
+    rover.y = rover.y - 1;
+    console.log( `The rover moved to ${rover.x}, ${rover.y}`);
+  } 
+  else if (rover.direction == 'W'){
+    rover.travelLog.push(rover.x , rover.y);
+    rover.x = rover.x - 1;
+    console.log( `The rover moved to ${rover.x}, ${rover.y}`);
+  }
+}
+
+
+function action(command){
+  for (i = 0; i < command.length; ++i) {
+    if (command.charAT(i) == 'f') {
+      moveForward(rover);
+    }
+    else if (command.charAT(i) == 'l'){
+      turnLeft(rover);
+    }
+    else if (command.charAT(i) == 'r'){
+      turnRight(rover);
+    }
+  }
 }
